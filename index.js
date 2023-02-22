a08bb7758d8f70dc9a14bd587e682a6ebec1b6c0
DinoDurakovic <113859620+DinoDurakovic@users.noreply.github.com>
2023-02-21 23:42:24 -0600

    trying out modals
    trying to code jquery modal with little success

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ $(".project1").on("mouseenter", function(){
     $(".project1").css("background-color", "grey")
 });
 
-document.getElementById("project1").addEventListener("mouseover", function(){
-    document.getElementById("project1").style.backgroundColor = "grey";
+$(".project1").on("mouseleave", function(){
+    $(".project1").css("background-color", "white")
 });
+
+$(".project2").on("mouseenter", function(){
+    $(".project2").css("background-color", "grey")
+});
+
+$(".project2").on("mouseleave", function(){
+    $(".project2").css("background-color", "white")
+});
+
+$(".project3").on("mouseenter", function(){
+    $(".project3").css("background-color", "grey")
+});
+
+$(".project3").on("mouseleave", function(){
+    $(".project3").css("background-color", "white")
+});
+
+$(".project4").on("mouseenter", function(){
+    $(".project4").css("background-color", "grey")
+});
+
+$(".project4").on("mouseleave", function(){
+    $(".project4").css("background-color", "white")
+});
+
+
 
 console.log("hello world.");
 
-$(".contact").on("click", function(){
-    $(".popup-content, .popup-overlay").addClass("active");
+$("popup1").on("click", function(){
+    $("popup1").addClass("active");
+});
+
+$('#popup1').popup({
+    background: false
 });
