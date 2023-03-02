3884e5a7c494fd781bfab462ae576f1accdd4431
DinoDurakovic <113859620+DinoDurakovic@users.noreply.github.com>
2023-03-01 23:56:36 -0600

    updates

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
 $(".project1").on("mouseenter", function(){
-    $(".project1").css("background-color", "grey")
+  $(".project1").css("background-color", "grey")
 });
 
 $(".project1").on("mouseleave", function(){
-    $(".project1").css("background-color", "white")
+  $(".project1").css("background-color", "white")
 });
 
 $(".project2").on("mouseenter", function(){
-    $(".project2").css("background-color", "grey")
+  $(".project2").css("background-color", "grey")
 });
 
 $(".project2").on("mouseleave", function(){
-    $(".project2").css("background-color", "white")
+  $(".project2").css("background-color", "white")
 });
 
 $(".project3").on("mouseenter", function(){
-    $(".project3").css("background-color", "grey")
+  $(".project3").css("background-color", "grey")
 });
 
 $(".project3").on("mouseleave", function(){
-    $(".project3").css("background-color", "white")
+  $(".project3").css("background-color", "white")
 });
 
 $(".project4").on("mouseenter", function(){
-    $(".project4").css("background-color", "grey")
+  $(".project4").css("background-color", "grey")
 });
 
 $(".project4").on("mouseleave", function(){
-    $(".project4").css("background-color", "white")
+  $(".project4").css("background-color", "white")
 });
+
+
+$("button1").on("mouseenter", function(){
+$("button1").css("background-color", "grey")
+});
+
+$("button1").on("mouseleave", function(){
+$("button1").css("background-color", "#30391A")
+});
+
+
+
+
 
 
 
@@ -47,17 +60,18 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+if (event.target == modal) {
+  modal.style.display = "none";
+}
 } 
+
